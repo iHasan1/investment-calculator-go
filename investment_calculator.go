bf6318e30148b5b32a3d0c1630e6076d41456dbb
Imran Hasan <imran.h94@gmail.com>
2024-02-01 12:03:15 +0500

    added alternative return syntax for functions

--- investment_calculator.go
+++ investment_calculator.go
@@ -23,8 +23,14 @@ func main() {
 	fmt.Println(futureRealValue)
 }
 
-func calcFutureValues (investmentAmount, expectedReturnRate, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	rfv := fv / math.Pow(1+inflationRate/100, years)
-	return fv, rfv
+// func calcFutureValues (investmentAmount, expectedReturnRate, years float64) (float64, float64) {
+// 	fv := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+// 	rfv := fv / math.Pow(1+inflationRate/100, years)
+// 	return fv, rfv
+// }
+
+func calcFutureValues (investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
+	return
 }
